e3a54e1fce9f2748a01cd7bcdfd23297ab447a7b
RAC IT <piyusha56@gmail.com>
2021-03-31 00:31:36 +0530

    changed / to ./ for test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
     <Switch>
-      <Route exact path="/" component={Main} />
+      <Route exact path="./" component={Main} />
       <Route component={NotFound} />
     </Switch>
   </Router>
